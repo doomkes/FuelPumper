46080bfa4989183c10f8d177e7dacd321517082e
Elijah <Elijah@elijahscomputer.ptd.local>
2017-02-14 21:22:13 -0500

    Added Autonomous Strategy #1 To Autonomous.cpp
    Created 2 Case's for the auto in Autonomous.cpp
    
    Case 0: 
    Move's the robot forward on each motor a X (-1 Thru 1) amount 
    for each motor for X amount of time (5 for now) then sets Auto 
    to Case 1
     
    Case 1:
    Stops the motor's and makes lastState = to m_autoState
    
    I added lastState, m_autoState, and m_autoStartTime in Autonomous.h

--- 2017_Robot/src/Autonomous.cpp
+++ 2017_Robot/src/Autonomous.cpp
@@ -13,16 +13,16 @@ using namespace grip;
 Autonomous::Autonomous(
 	CameraServer *m_cameraServer,
 		cs::CvSource &m_outputStream,
-		cs::UsbCamera &camera
-//		,
-//		grip::BoilerVision &m_boilerVision
-		)
-:
+		cs::UsbCamera &camera,
+		TankDrive &tankDrive
+		// grip::BoilerVision & m_boilerVision
+		):
 
 		m_cameraServer(m_cameraServer),
 		m_outputStream(m_outputStream),
 		camera(camera),
-		m_boilerVision(m_boilerVision)
+		m_boilerVision(m_boilerVision),
+		m_tank(tankDrive)
 {
 
 }
@@ -37,42 +37,70 @@ void Autonomous::AutonomousInit() {
 		m_outputStream = CameraServer::GetInstance()->PutVideo("thresh", 640, 480);
 		camera.SetResolution(640, 480);
 		camera.SetExposureManual(1);
+
+		m_autoStartTime = Timer::GetFPGATimestamp();
 	}
 
 void Autonomous::AutonomousPeriodic() {
-//		cv::Mat frame;
-//
-//		m_cameraServer->GetVideo().GrabFrame(frame);
-//		m_boilerVision.process(frame);
-//		std::vector<std::vector<cv::Point>> foundContours;
-//		foundContours = *m_boilerVision.getfindContoursOutput();
-//
-//		vector<cv::Moments> mu(foundContours.size());
-//
-//		vector<cv::Point2f> mc(foundContours.size());
-//
-//		for( unsigned i = 0; i < foundContours.size(); i++ ) {
-//			mu[i] = moments( foundContours[i], false );
-//		}
-//
-//		for(unsigned i = 0; i < foundContours.size(); i++) {
-//			mc[i] = cv::Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
-//		}
-//
-//		for(unsigned i = 0; i < foundContours.size(); i++) {
-//			printf("Found Contours!:  x: %f, y: %f\n", mc[i].x, mc[i].y);
-//		}
-//		for(unsigned i = 0; i < foundContours.size(); i++) {
-//			cv::Rect boundingRect = cv::boundingRect(foundContours[i]);
-//			printf("X: %i, Y: %i, W: %i, H: %i\n",boundingRect.x,boundingRect.y,boundingRect.width,boundingRect.height);
-//		}
-//
-//
-//		cv::Mat contourImg;
-//		cv::Mat mymat;
-//		for(auto contour : foundContours) {
-//			cv::drawContours(contourImg, contour, {255, 0, 0});
-//			cv::Mat mymat = *Vision.gethsvThresholdOutput();
-//		}
-//		m_outputStream.PutFrame(mymat);
+
+	AutoStrategy1();
+
+	//	cv::Mat frame;
+
+	//	m_cameraServer->GetVideo().GrabFrame(frame);
+	//	m_boilerVision.process(frame);
+	//	std::vector<std::vector<cv::Point>> foundContours;
+	//	foundContours = *m_boilerVision.getfindContoursOutput();
+
+	//	vector<cv::Moments> mu(foundContours.size());
+
+	//	vector<cv::Point2f> mc(foundContours.size());
+
+	//	for( unsigned i = 0; i < foundContours.size(); i++ ) {
+	//		mu[i] = moments( foundContours[i], false );
+	//	}
+
+	//	for(unsigned i = 0; i < foundContours.size(); i++) {
+	//		mc[i] = cv::Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+	//	}
+
+	//	for(unsigned i = 0; i < foundContours.size(); i++) {
+	//		printf("Found Contours!:  x: %f, y: %f\n", mc[i].x, mc[i].y);
+	//	}
+	//	for(unsigned i = 0; i < foundContours.size(); i++) {
+	//		cv::Rect boundingRect = cv::boundingRect(foundContours[i]);
+	//		printf("X: %i, Y: %i, W: %i, H: %i\n",boundingRect.x,boundingRect.y,boundingRect.width,boundingRect.height);
+	//	}
+
+
+	//	cv::Mat contourImg;
+	//	cv::Mat mymat;
+	//	for(auto contour : foundContours) {
+	//		cv::drawContours(contourImg, contour, {255, 0, 0});
+	//		cv::Mat mymat = *Vision.gethsvThresholdOutput();
+	//	}
+	//	m_outputStream.PutFrame(mymat);
 	}
+void Autonomous::AutoStrategy1() {
+
+	double currentAutoTime = Timer::GetFPGATimestamp() - m_autoStartTime;
+
+	switch (m_autoState){
+
+		case 0: // Retrieves 50 fuel from hopper then shoots in high goal
+			if (m_autoState != lastState) {
+				m_tank.Drive(.1 , .1);
+			}
+			if (currentAutoTime >= 5){
+				m_autoState = m_autoState++;
+			}
+			break;
+
+		case 1: // Stop Driving
+			if (m_autoState != lastState) {
+				m_tank.Drive(0 , 0);
+			}
+			lastState = m_autoState;
+			break;
+	}
+}
--- 2017_Robot/src/Autonomous.h
+++ 2017_Robot/src/Autonomous.h
@@ -9,27 +9,34 @@
 #ifndef SRC_AUTONOMOUS_H_
 #define SRC_AUTONOMOUS_H_
 
+#include "TankDrive.h"
 #include "BoilerVision.h"
 using namespace cs;
 using namespace grip;
+
 class Autonomous {
 	CameraServer *m_cameraServer = nullptr;
 	cs::CvSource m_outputStream;
 	cs::UsbCamera camera;
 	grip::BoilerVision m_boilerVision;
+	TankDrive & m_tank;
+	double m_autoStartTime = 0;
+	unsigned m_autoState = 0;
+	unsigned lastState = 99;
 
 public:
 	Autonomous(
 		CameraServer *,
 		CvSource &m_outputStream,
-		UsbCamera &camera
-//		,
-//		BoilerVision &m_boilerVision
+		UsbCamera &camera,
+		TankDrive&
+//		BoilerVision & m_boilerVision
 	);
 	virtual ~Autonomous();
 
 	void AutonomousPeriodic();
 	void AutonomousInit();
+	void AutoStrategy1();
 };
 
 #endif /* SRC_AUTONOMOUS_H_ */
