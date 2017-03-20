afb1c640f63be7ba6051a1d3ffd34630b0aeb2c7
markjjordan <mark@markjordan.org>
2017-03-20 19:42:36 -0400

    Merged in IDE.

--- 2017_Robot/src/Autonomous.h
+++ 2017_Robot/src/Autonomous.h
@@ -1,71 +1,3 @@
-====== master
-/*
- * Autonomous.h
- *
- *  Created on: Feb 13, 2017
- *      Author: Joey
- */
-#include <RelativeMovement.h>
-#include <WPILib.h>
-
-#ifndef SRC_AUTONOMOUS_H_
-#define SRC_AUTONOMOUS_H_
-#include "TankDrive.h"
-#include "BoilerVision.h"
-#include "TrapezoidalMove.h"
-#include "GearManipulator.h"
-using namespace cs;
-using namespace grip;
-
-enum AutoMode {
-	DO_NOTHING,
-	DIAGONAL_HOPPER_SHOOT,
-	ARC_HOPPER_SHOOT,
-	CENTER_GEAR_BASELINE,
-	BASE_LINE,
-	POS_TEST = 99,
-	VBUS_TEST = 98,
-};
-
-class Autonomous {
-	CameraServer *m_cameraServer = nullptr;
-	cs::CvSource m_outputStream;
-	cs::UsbCamera camera;
-	grip::BoilerVision m_boilerVision;
-	TankDrive &m_tank;
-	Shooter &m_shooter;
-	GearManipulator& m_gear;
-	ChainCommands m_chain;
-	RelativeMovement m_relMove;
-	AutoMode m_mode = DO_NOTHING;
-	int m_state = 0;
-	float m_startAngle = 0;
-	TrapezoidalMove m_move;
-	Timer m_timer;
-public:
-	Autonomous(
-		CameraServer *,
-		CvSource &m_outputStream,
-		UsbCamera &camera,
-		TankDrive &m_tank,
-		Shooter &shooter,
-		GearManipulator&
-	);
-	virtual ~Autonomous();
-
-	void AutonomousPeriodic();
-	void AutonomousInit();
-
-	void StraightGear();
-	void ShootFromHopper();
-	void ArcShootFromHopper();
-	void PositionTest();
-	void VBusTest();
-	void BaseLine();
-};
-
-#endif /* SRC_AUTONOMOUS_H_ */
-=======
 /*
  * Autonomous.h
  *
@@ -91,6 +23,7 @@ enum AutoMode {
 	CENTER_GEAR_BASELINE,
 	ARC_SIDE_GEAR,
 	STATIONARY_SHOOT,
+	BASE_LINE,
 	POS_TEST = 99,
 	VBUS_TEST = 98,
 };
@@ -131,7 +64,8 @@ public:
 	void PositionTest();
 	void VBusTest();
 	void StationaryShoot();
+	void BaseLine();
+
 };
 
 #endif /* SRC_AUTONOMOUS_H_ */
-====== master
